55c86523ec62c7b0c7e3da302a733736db7a7e96
ikvarxt <leonekola@outlook.com>
2025-03-10 23:30:45 +0800

    add ts-autotag

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,7 +2,9 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+  },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
